2c2dd9bc5bc7f7bae33710e41ae2120cb073d662
jasonelliots <j.elliotschwartz@gmail.com>
2020-08-14 10:03:09 -0700

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = [i for i in range(capacity)]
 
     def append(self, item):
-        pass
-
+        self.data.pop(0)
+        self.data.append(item)
+            
     def get(self):
-        pass
+        return self.data
+
+# class RingBuffer:
+
+#     def __init__(self,size_max):
+#         self.max = size_max
+#         self.data = []
+
+#     def append(self,x):
+#         self.data.append(x)
+#         if len(self.data) == self.max:
+#             self.data.pop(0)
+
+#     def get(self):
+#         return self.data
