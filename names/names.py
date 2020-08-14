7a9f485d26e1f0406d41d44f82f6187ba71c4d96
jasonelliots <j.elliotschwartz@gmail.com>
2020-08-14 09:50:22 -0700

    ring buffer

--- names/names.py
+++ names/names.py
@@ -10,13 +10,39 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates = set()  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+combined = set()
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    combined.add(name_1)
+
+for name_2 in names_2:
+    combined.add(name_2)
+
+for name in combined:
+    if name in names_2 and name in names_1:
+        duplicates.add(name)
+
+
+# combined = set()
+# for name_1 in names_1:
+#     combined.add(name_1)
+
+# for name_2 in names_2:
+#     combined.add(name_2)
+
+# for name in names_2:
+#     if name not in combined:
+#         duplicates.add(name)
+
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.add(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
